b5e0819b5ec5a5a43d67a1a99589b85ee02e2575
Helen Koo <helenkoo2017@gmail.com>
2022-12-21 13:48:24 -0800

    add SpotImages table migration and seeds

---
+++ backend/db/seeders/20221221175619-demo-spotImage.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    options.tableName = "SpotImages";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          url: "image url1",
+          preview: true,
+          spotId: 1,
+        },
+        {
+          url: "image url2",
+          preview: true,
+          spotId: 2,
+        },
+        {
+          url: "image url3",
+          preview: true,
+          spotId: 3,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = "SpotImages";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: { [Op.in]: [1, 2, 3] },
+      },
+      {}
+    );
+  },
+};
