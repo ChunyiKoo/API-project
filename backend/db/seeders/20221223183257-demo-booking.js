e981f279b0b4ab57f977cdddc309dd272f6b71e9
Helen Koo <helenkoo2017@gmail.com>
2023-01-24 15:44:15 -0800

    seed file of booking

--- backend/db/seeders/20221223183257-demo-booking.js
+++ backend/db/seeders/20221223183257-demo-booking.js
@@ -88,13 +88,14 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
     options.tableName = "Bookings";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {
-        id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
-      },
-      {}
-    );
+    await queryInterface.bulkDelete(options, null, {});
+    // const Op = Sequelize.Op;
+    // return queryInterface.bulkDelete(
+    //   options,
+    //   {
+    //     id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
+    //   },
+    //   {}
+    // );
   },
 };
