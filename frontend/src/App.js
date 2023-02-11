852a46ab939f5b446bbe6e1e05f78d28762aaab0
Helen Koo <helenkoo2017@gmail.com>
2023-02-10 22:11:16 -0800

    Revert "finish phase four"
    This reverts commit 9362168aeb2ed4b1ad43eabe8869e40f4e87f6c7.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+// frontend/src/App.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -12,10 +14,16 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && <Switch></Switch>}
-    </>
+    isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,15 +60,6 @@ export const signup = (user) => async (dispatch) => {
 };
 // ...
 
-// logout thunk action
-export const logout = () => async (dispatch) => {
-  const response = await csrfFetch("/api/session", {
-    method: "DELETE",
-  });
-  dispatch(removeUser());
-  return response;
-};
-
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
